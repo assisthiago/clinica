d13ab808ed5435bdb29922d04b9b52af933133ca
Thiago Assis <thiago.medassis@gmail.com>
2022-04-30 18:04:12 -0300

    Sign Up

--- resources/auth/views.py
+++ resources/auth/views.py
@@ -6,3 +6,7 @@ auth_bp = Blueprint('auth', __name__)
 @auth_bp.route('/signin', methods=['GET'])
 def get_signin():
     return render_template('auth/signin.html')
+
+@auth_bp.route('/signup', methods=['GET'])
+def get_signup():
+    return render_template('auth/signup.html')
