c27f68e97bc29b198231862d7b81f2d4e5734cc8
Thiago Assis <thiago.medassis@gmail.com>
2022-04-30 19:13:31 -0300

    Heroku setup

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import Flask, render_template
-from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -14,40 +13,19 @@ def page_not_found(e):
 def page_internal_server_error(e):
     return render_template('500.html'), 404
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__)
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRECT_KEY'] = os.urandom(12).hex()
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    db.init_app(app)
 
     with app.app_context():
         db.init_app(app)
         migrate.init_app(app, db)
 
-    # login_config(app)
-    register_blueprints(app)
     app.register_error_handler(404, page_not_found)
     app.register_error_handler(500, page_internal_server_error)
 
     return app
 
-def login_config(app):
-    # from auxiliar.models import Auxiliar
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.signin'
-    login_manager.init_app(app)
-
-    # @login_manager.user_loader
-    # def load_user(auxiliar_id):
-    #     return Auxiliar.query.get(int(auxiliar_id))
-
-def register_blueprints(app):
-    from resources.auth.views import auth_bp
-
-    app.register_blueprint(auth_bp)
--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,3 @@
-from . import app
-app.db.create_all(app=app.create_app)
+from app import app
+
+app.db.init_app(app=app.create_app())
